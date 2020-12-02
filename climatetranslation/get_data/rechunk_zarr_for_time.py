04575b6db302c2cb1bbd80194f3fb06430702c13
James Fulton <djamesfulton@yahoo.co.uk>
2020-12-02 11:41:12 +0000

    minimal working version

--- climatetranslation/get_data/rechunk_zarr_for_time.py
+++ climatetranslation/get_data/rechunk_zarr_for_time.py
@@ -5,9 +5,12 @@ from rechunker import rechunk
 import argparse
 import time
 import os
+import re
 
 from dask.distributed import Client
 from dask.diagnostics import ProgressBar
+
+import progressbar
 
 from climatetranslation.unit.data import construct_regridders
 
@@ -88,10 +91,8 @@ args.regridintermediatezarr = filepathcheck.regridintermediatezarr(args.regridin
 class ob: pass
 args = ob()
 args.latlonchunks = 4
-args.n_times = 8
 args.inputzarr = ['/datadrive/hadgem3/nat_hist_zarr', '/datadrive/cam5/nat_hist_zarr']
-args.rechunkintermediatezarr = ['/datadrive/hadgem3/trans5_rechunk_intermediate', '/datadrive/cam5/trans5_rechunk_intermediate']
-args.regridintermediatezarr = ['/datadrive/hadgem3/trans5_regrid_intermediate', '/datadrive/cam5/trans5_regrid_intermediate']
+args.intermediatezarr = ['/datadrive/hadgem3/trans5_intermediate', '/datadrive/cam5/trans5_intermediate']
 args.outputzarr = ['/datadrive/hadgem3/trans5', '/datadrive/cam5/trans5']
 args.max_mem = '20GB'
 args.n_workers = 1
@@ -102,35 +103,41 @@ if args.n_workers>1:
     client = Client(n_workers=args.n_workers, threads_per_worker=1,)# memory_limit=f'{args.max_mem//args.n_workers}GB')
     
 datasets = [xr.open_zarr(f, consolidated=True) for f in args.inputzarr]
+
 # regrid to common grid if nore than 1 dataset
 if len(datasets)>1:
     regridders = [*construct_regridders(*datasets)]
     datasets = [ds if rg is None else rg(ds) for ds, rg in zip(datasets, regridders)]
-
     
-for ds, outputzarr, regrid_temp_store, rechunk_temp_store in zip(datasets, args.outputzarr, args.regridintermediatezarr, args.rechunkintermediatezarr):
-    break
-    # save out the regridded data if regridding is peformed
-    if len(datasets)>1:
-        with ProgressBar():
-            ds.to_zarr(regrid_temp_store, consolidated=True)
-        ds = xr.open_zarr(regrid_temp_store, consolidated=True)
 
-    target_chunks = dict(run=1, time=1, lat=args.latlonchunks, lon=args.latlonchunks, height=1)
-    target_chunks_dict = {k:tuple([target_chunks[d] for d in ds[k].dims]) for k in ds.keys()}
+def parse_size(size):
+    units = {"B": 1, "KB": 1e3, "MB": 1e6, "GB": 1e9}
+    number, unit = re.findall(r'[A-Za-z]+|\d+', size)
+    return int(float(number)*units[unit])
 
-    # Compute rechunking method
-    array_plan = rechunk(ds, target_chunks_dict, args.max_mem, outputzarr, temp_store=rechunk_temp_store)
+max_bytes = parse_size(args.max_mem)   
+
+for ds, outputzarr, temp_store in zip(datasets, args.outputzarr, args.intermediatezarr):
+    n_times = 2*ds.nbytes//max_bytes
+    dn = len(ds.time)//(n_times-1)
+    
+    mode="w-"
+    append_dim=None
+    for i in progressbar.progressbar(range(n_times)):
+        ds.isel(time=slice(i*dn, (i+1)*dn)) \
+            .chunk(dict(lat=args.latlonchunks, lon=args.latlonchunks, time=dn)) \
+            .to_zarr(temp_store, mode=mode, append_dim=append_dim, consolidated=True)
+        mode="a"
+        append_dim = "time"
+    
+    load_chunks = {k:v[0] for k,v in ds.chunks.items()}
+    load_chunks.update(dict(lat=args.latlonchunks, lon=args.latlonchunks,time=-1))
+    ds = xr.open_zarr(temp_store, consolidated=True, chunks=load_chunks)
 
     with ProgressBar():
-        array_plan.execute()
+        ds.to_zarr(outputzarr, consolidated=True)
 
-    os.system(f"rm -rf {rechunk_temp_store}")
-    if len(datasets)>1:
-        os.system(f"rm -rf {regrid_temp_store}")
-
-    # This next section manually consolidates the zarr store
-    zarr.convenience.consolidate_metadata(outputzarr)
+    os.system(f"rm -rf {temp_store}")
 
 if args.n_workers>1:
     client.close()
